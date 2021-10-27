ae010ab8a7b359864c317358f636a1b329d87642
3vau <3vau@sina.cn>
2021-10-27 18:33:39 +0800

    Update

--- themes/main.js
+++ themes/main.js
@@ -1,7 +1,6 @@
 'use strict';
 import 'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js';
 import 'https://cdn.jsdelivr.net/npm/darkreader@4.9.34/darkreader.min.js';
-import 'https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
 import userconfig from '../user.config.js';
 import {
     isHome,
